db3283c6c4a93c4cab7115b2b6f17ce0d8f75dee
Nikhil Sengar <nsengar@modeln.com>
2020-06-08 20:46:12 +0530

    + Corrected the functionality of button click and the key press. + rearranged the style.css file. + made some changes to drum-roll.html

--- js/index.js
+++ js/index.js
@@ -1,37 +1,94 @@
+// function keyPressOrBtnClick(event) {
+// //   var keyPress = event.which || event.keyCode;
+// //   if (keyPress == 87) {
+//     document.querySelector('.w').addEventListener('keydown', function () {
+//       new Audio('../sounds/tom-1.mp3').play();
+//     });
+//   } else {
+//     document.querySelector('.w').addEventListener('click', function () {
+//       new Audio('../sounds/tom-1.mp3').play();
+//     });
+//   }
+// //   document.querySelector('.a').addEventListener('keydown', function () {
+//     new Audio('../sounds/tom-2.mp3').play();
+//   });
+// //   document.querySelector('.s').addEventListener('keydown', function () {
+//     new Audio('../sounds/tom-3.mp3').play();
+//   });
+// //   document.querySelector('.d').addEventListener('keydown', function () {
+//     new Audio('../sounds/tom-4.mp3').play();
+//   });
+// //   document.querySelector('.j').addEventListener('keydown', function  () {
+//     new Audio('../sounds/snare.mp3').play();
+//   });
+// //   document.querySelector('.k').addEventListener('keydown', function () {
+//     new Audio('../sounds/crash.mp3').play();
+//   });
+// //   document.querySelector('.l').addEventListener('keydown', function () {
+//     new Audio('../sounds/kick-bass.mp3').play();
+//   });
+// }
 
-document.querySelector('.w').addEventListener('keydown', function (event) {
-  if (event.keyCode == 87) {
-    // event.preventDefault();
-    new Audio('../sounds/tom-1.mp3').play();
+// Taking number of buttons
+var noOfDrumButtons = document.querySelectorAll('.drum').length;
+
+// onClick button make sound option
+for (var i = 0; i < noOfDrumButtons; i++)
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    this.style.color = 'white';
+    var btnInnerHTML = this.innerHTML;
+    makeSound(btnInnerHTML);
+  });
+
+// OnKeyPress make sound option
+document.addEventListener('keypress', function () {
+    makeSound(event.key);
+  });
+
+// makeSound() function which detect the key press or
+// the button click and then play sound
+function makeSound(key) {
+
+  switch (key) {
+    case 'w':
+      new Audio('sounds/tom-1.mp3').play();
+      break;
+    case 'a':
+      new Audio('../sounds/tom-2.mp3').play();
+      break;
+    case 's':
+      new Audio('../sounds/tom-3.mp3').play();
+      break;
+    case 'd':
+      new Audio('../sounds/tom-4.mp3').play();
+      break;
+    case 'j':
+      new Audio('../sounds/snare.mp3').play();
+      break;
+    case 'k':
+      new Audio('../sounds/crash.mp3').play();
+      break;
+    case 'l':
+      new Audio('../sounds/kick-bass.mp3').play();
+      break;
+    default: console.log(key);
+
   }
-});
+}
 
-document.querySelector('.a').addEventListener('keyup', function (event) {
-  if (event.keyCode == 65)
-    new Audio('../sounds/tom-2.mp3').play();
-});
-
-document.querySelector('.s').addEventListener('keyup', function (event) {
-  if (event.keyCode == 83)
-    new Audio('../sounds/tom-3.mp3').play();
-});
-
-document.querySelector('.d').addEventListener('keyup', function (event) {
-  if (event.keyCode == 68)
-    new Audio('../sounds/tom-4.mp3').play();
-});
-
-document.querySelector('.j').addEventListener('keyup', function (event) {
-  if (event.keyCode == 74)
-    new Audio('../sounds/snare.mp3').play();
-});
-
-document.querySelector('.k').addEventListener('keyup', function (event) {
-  if (event.keyCode == 75)
-    new Audio('../sounds/crash.mp3').play();
-});
-
-document.querySelector('.l').addEventListener('keyup', function (event) {
-  if (event.keyCode == 76)
-    new Audio('../sounds/kick-bass.mp3').play();
-});
+// document.querySelector('.set').addEventListener('keyup', function (event) {
+//   if (event.keyCode == 87)
+//     new Audio('../sounds/tom-1.mp3').play();
+//   else if (event.keyCode == 65)
+//     new Audio('../sounds/tom-2.mp3').play();
+//   else if (event.keyCode == 83)
+//     new Audio('../sounds/tom-3.mp3').play();
+//   else if (event.keyCode == 68)
+//     new Audio('../sounds/tom-4.mp3').play();
+//   else if (event.keyCode == 74)
+//     new Audio('../sounds/snare.mp3').play();
+//   else if (event.keyCode == 75)
+//     new Audio('../sounds/crash.mp3').play();
+//   else if (event.keyCode == 76)
+//     new Audio('../sounds/kick-bass.mp3').play();
+// });
